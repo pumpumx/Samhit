bad572b15ecd4267a4484dbeead2bccbeccb0f67
pumpumx <trishuchibhot14@gmail.com>
2025-06-20 14:47:35 +0530

    Initialising frontend

---
+++ FrontEnd/src/app.tsx
@@ -0,0 +1,10 @@
+
+export default function MainApp(){
+    return(
+        <>
+            <div>
+                <h1>Yo , lets start the project</h1>
+            </div>
+        </>
+    )
+}
---
+++ FrontEnd/src/main.tsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import MainApp from './app'
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <MainApp/>
+  </StrictMode>
+)
